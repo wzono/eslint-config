70d899c150d50a96b55d1523daab4678ed8accb7
wzono <wzono.me@gmail.com>
2023-06-09 10:12:05 +0800

    feat: add react config

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,36 @@
+const { isPackageExists } = require('local-pkg')
+
+const TS = isPackageExists('typescript')
+
+if (!TS)
+  console.warn('[@wzono/eslint-config] TypeScript is not installed, fallback to JS only.')
+
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    TS ? '@wzono/eslint-config-ts' : '@wzono/eslint-config-basic',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'jsx-quotes': [
+      'error',
+      'prefer-double',
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-first-prop-new-line': ['error', 'multiline-multiprop'],
+    'react/jsx-props-no-multi-spaces': ['error'],
+    'react/jsx-one-expression-per-line': ['error'],
+    'react/jsx-closing-bracket-location': ['error'],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-max-props-per-line': [
+      'error',
+      { maximum: 1, when: 'multiline' },
+    ],
+  },
+}
